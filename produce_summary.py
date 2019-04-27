e613efbb1a736ea661a7d94f4605edd2f9eb3ee3
vagrant <vagrant@vagrant.vm>
2019-04-27 05:41:34 +0000

    update function and comments

--- produce_summary.py
+++ produce_summary.py
@@ -1,21 +1,24 @@
-file_one = "um-deliveries-20140519.txt"
-file_two = "um-deliveries-20140520.txt"
-file_three = "um-deliveries-20140521.txt"
 
-def produce_summary(day, file):
 
-        print(day)
+def produce_summary(day_number, file):
+        """Creates report when given day and file"""
+
+        print("Day", day_number)
         the_file = open(file)
         for line in the_file:
-        line = line.rstrip()
-        words = line.split('|')
+                line = line.rstrip()
+                words = line.split('|')
 
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
+                melon = words[0]
+                count = words[1]
+                amount = words[2]
 
         print("Delivered {} {}s for total of ${}".format(
-                count, melon, amount))
+                        count, melon, amount))
         the_file.close()
 
+
+produce_summary(1 ,"um-deliveries-20140519.txt") 
+produce_summary(2 ,"um-deliveries-20140520.txt") 
+produce_summary(3 ,"um-deliveries-20140521.txt") 
 
